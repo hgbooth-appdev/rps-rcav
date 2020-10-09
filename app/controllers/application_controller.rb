0b4165ff2de71e2bfc173f2e47055741863d5dfe
hgbooth <hgregor0@chicagobooth.edu>
2020-10-09 19:34:32 +0000

    Lots of paper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,34 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def homepage
+    # write code to do work
+    render({ :template => "game_templates/rules.html.erb"})
+  end
+
   def play_rock
     # write code to do work
-    
-
-
-    # redirect_to("https://www.wikipedia.org")
     render({ :template => "game_templates/user_rock.html.erb"})
+  end
+  
+  def play_paper
+    # write code to do work
+    @compMove = ["rock", "paper", "scissors"].sample 
+    
+    if @compMove == "rock" 
+      @outcome = "We won!"
+    elsif @compMove == "paper" 
+      @outcome = "We tied!"
+    elsif @compMove == "scissors" 
+      @outcome = "We lost!"
+    end 
+
+    render({ :template => "game_templates/user_paper.html.erb"})
+  end
+  
+  def play_scissors
+    # write code to do work
+    render({ :template => "game_templates/user_scissors.html.erb"})
   end
 
 end
